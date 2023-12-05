b9eb3994c2dcc467457a99a5765e40c24a20fe8c
Rob Tillaart <rob.tillaart@gmail.com>
2023-12-05 15:42:10 +0100

    remove ESP32 specific begin() (#24)
    - remove ESP32 specific **begin()**
    - add (experimental) support or AD5280 / AD5282.
    - fix #23 in readme
    - update and add examples.

--- examples/AD5241_write/AD5241_write.ino
+++ examples/AD5241_write/AD5241_write.ino
@@ -1,14 +1,14 @@
 //
-//    FILE: AD524X_write_AD5241ino.ino
+//    FILE: AD5241_write.ino
 //  AUTHOR: Rob Tillaart
-// PURPOSE: AD524X demo program
+// PURPOSE: AD5241 demo program - single channel!
 //     URL: https://github.com/RobTillaart/AD524X
 //
 
 
 #include "AD524X.h"
 
-AD5241 AD01(0x2C);  // AD0 & AD1 == GND
+AD5280 AD01(0x2C);  //  AD0 & AD1 == GND
 
 
 void setup()
@@ -16,7 +16,7 @@ void setup()
   Serial.begin(115200);
   Serial.println();
   Serial.println(__FILE__);
-  Serial.println();
+  Serial.print("AD524X_LIB_VERSION: ");
   Serial.println(AD524X_LIB_VERSION);
 
   Wire.begin();
@@ -25,8 +25,6 @@ void setup()
   bool b = AD01.begin();
   Serial.println(b ? "true" : "false");
   Serial.println(AD01.isConnected());
-
-  Serial.println(AD01.pmCount());
 }
 
 
@@ -34,14 +32,14 @@ void loop()
 {
   for (int val = 0; val < 255; val++)
   {
-    AD01.write(0, val);
+    AD01.write(val);
     if (val == 200)
     {
-      AD01.write(1, val, HIGH, LOW);
+      AD01.write(val, HIGH, LOW);
     }
     if (val == 0)
     {
-      AD01.write(0, val, LOW, LOW);
+      AD01.write(val, LOW, HIGH);
     }
     Serial.println(val);
     delay(20);
@@ -50,4 +48,3 @@ void loop()
 
 
 //  -- END OF FILE --
-
